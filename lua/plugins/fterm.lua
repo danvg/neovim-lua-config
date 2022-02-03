25b7a1f4b3a5e4b52bb72692ba27110731981d1d
Dan Lee Vazquez Garcia <dava1000@student.miun.se>
2022-02-03 10:15:27 +0100

    Better keymap for terminal

--- lua/plugins/fterm.lua
+++ lua/plugins/fterm.lua
@@ -12,11 +12,16 @@ fterm.setup({
   dimensions = { height = 0.8, width = 0.8, x = 0.5, y = 0.5 }
 })
 
-vim.cmd [[
-  noremap <silent> <leader>tt :lua require("FTerm").toggle()<CR>
-  noremap <silent> <leader>tk :lua require("FTerm").exit()<CR>
-  noremap <silent> <leader>tg :lua require("FTerm").run("lazygit")<CR>
+local opt = { silent = true, noremap = true }
+local map = vim.api.nvim_set_keymap
 
+map("n", "<leader>tt", "<cmd>lua require('FTerm').toggle()<CR>", opt)
+map("t", "<leader>tt", "<C-\\><C-n><cmd>lua require('FTerm').toggle()<CR>", opt)
+map("n", "<leader>te", "<cmd>lua require('FTerm').exit()<CR>", opt)
+map("t", "<leader>te", "<C-\\><C-n><cmd>lua require('FTerm').exit()<CR>", opt)
+map("n", "<leader>tg", "<cmd>lua require('FTerm').run('lazygit')<CR>", opt)
+
+vim.cmd [[
   command! FTermOpen lua require("FTerm").open()
   command! FTermClose lua require("FTerm").close()
   command! FTermExit lua require("FTerm").exit()
