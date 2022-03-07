7800d3994bd0eca8aa02a797e1e5b4461998eb19
Dan Lee Vazquez Garcia <dava1000@student.miun.se>
2022-03-07 13:57:48 +0100

    Add experimental web search command.

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,7 +1,27 @@
-function _G.StripTrailingWhitespaces()
+function _G.StripTrailingSpaces()
   local save = vim.fn.winsaveview()
   vim.cmd [[%s/\v\s+$//e]]
   vim.fn.winrestview(save)
 end
 
-vim.cmd [[:command! StripTrailingWhitespaces v:lua.StripTrailingWhitespaces()]]
+vim.cmd [[command! StripTrailingSpaces lua _G.StripTrailingSpaces()]]
+
+local url_prefix = "http://www.google.com/search?q="
+
+function _G.WebSearch(term)
+  local url = url_prefix .. term
+  if vim.fn.has("Win32") then
+    vim.loop
+      .spawn("rundll32", { args = { "url.dll,FileProtocolHandler", url } })
+  else
+    vim.loop.spawn("xdg-open", { args = { url } })
+  end
+end
+
+function _G.WebSearchCW()
+  local cword = vim.fn.expand("<cword>")
+  _G.WebSearch(cword);
+end
+
+vim.cmd [[command! -nargs=1 WebSearch lua _G.WebSearch(<f-args>)]]
+vim.cmd [[command! WebSearchCW lua _G.WebSearchCW()]]
