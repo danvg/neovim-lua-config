ce319fae48eaa58f211bd4ceea374a8c6440f041
Dan Lee Vazquez Garcia <dava1000@student.miun.se>
2022-04-29 10:57:32 +0200

    Update config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ if not configs_ok then
 end
 
 configs.setup({
-  ensure_installed = "maintained",
+  ensure_installed = "all",
   highlight = { enable = true },
   indent = { enable = true },
   rainbow = { enable = true }
