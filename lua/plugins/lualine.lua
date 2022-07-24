4cd01e67fff90f715497234c5d77005322745e2c
Dan Lee Vazquez Garcia <dava1000@student.miun.se>
2022-07-24 16:06:40 +0200

    Update lualine config. Use the new winbar.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,30 +4,67 @@ if not lualine_ok then
   return
 end
 
+local breadcrumbs
 local navic_ok, navic = pcall(require, "nvim-navic")
-
-local section_c
 if navic_ok then
-  local function breadcrumbs()
-    if navic.is_available() then return navic.get_location() else return "" end
+  breadcrumbs = function()
+    if navic.is_available() then
+      return navic.get_location({})
+    else
+      return ""
+    end
   end
-
-  section_c = { "filename", { breadcrumbs } }
 else
-  section_c = { "filename" }
+  vim.notify("navic module not found!")
+  breadcrumbs = ""
 end
 
 lualine.setup {
-  options = { theme = "tokyonight" },
+  options = {
+    icons_enabled = true,
+    theme = "github_dimmed",
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+    disabled_filetypes = { statusline = { "NvimTree" }, winbar = { "NvimTree " } },
+    always_divide_middle = true,
+    globalstatus = true,
+    refresh = { statusline = 1000, tabline = 1000, winbar = 1000 }
+  },
   sections = {
     lualine_a = { "mode" },
     lualine_b = { "branch", "diff" },
-    lualine_c = section_c,
+    lualine_c = { "filename" },
     lualine_x = {
       { "diagnostics", sources = { "nvim_diagnostic" } }, "encoding",
       "fileformat", "filetype"
     },
     lualine_y = { "progress" },
     lualine_z = { "location" }
-  }
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { "filename" },
+    lualine_x = { "location" },
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {
+    lualine_a = { "filename" },
+    lualine_b = { breadcrumbs },
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  inactive_winbar = {
+    lualine_a = { "filename" },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  extensions = {}
 }
