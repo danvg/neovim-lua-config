8c0be590a5c156c185d2a01410928a1de2000b09
Dan Lee Vazquez Garcia <dava1000@student.miun.se>
2022-10-20 21:03:49 +0200

    Fix wrong server name

--- lua/config/plugin/mason-lspconfig.lua
+++ lua/config/plugin/mason-lspconfig.lua
@@ -2,6 +2,4 @@ if not packer_plugins["mason.nvim"].loaded then
   vim.cmd([[packadd mason.nvim]])
 end
 
-require("mason-lspconfig").setup({
-  ensure_installed = { "lua-language-server" },
-})
+require("mason-lspconfig").setup()
