4fb8098955db1cbeeec97cc2f17afaebad2c3665
Dan Lee Vazquez Garcia <dava1000@student.miun.se>
2023-02-01 15:37:18 +0100

    Lazyload neo-tree

--- lua/config/plugins/neo-tree.lua
+++ lua/config/plugins/neo-tree.lua
@@ -6,8 +6,11 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
+  cmd = "Neotree",
+  keys = "<leader>e",
   config = function()
     vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
     require("neo-tree").setup({})
+    vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>")
   end,
 }
