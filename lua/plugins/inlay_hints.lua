9a1fe3fbdcc4457ce77e834b9afd6acb8d5f9339
Dan Lee Vazquez Garcia <dava1000@student.miun.se>
2023-02-06 18:17:45 +0100

    Add inlay hints commands.

--- lua/plugins/inlay_hints.lua
+++ lua/plugins/inlay_hints.lua
@@ -3,9 +3,8 @@ return {
   lazy = true,
   config = true,
   init = function()
-    vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
     vim.api.nvim_create_autocmd("LspAttach", {
-      group = "LspAttach_inlayhints",
+      group = vim.api.nvim_create_augroup("LspAttach_inlayhints", {}),
       callback = function(args)
         if not (args.data and args.data.client_id) then
           return
@@ -13,7 +12,38 @@ return {
 
         local bufnr = args.buf
         local client = vim.lsp.get_client_by_id(args.data.client_id)
-        require("lsp-inlayhints").on_attach(client, bufnr)
+        require("lsp-inlayhints").on_attach(client, bufnr, false)
+
+        vim.api.nvim_buf_create_user_command(bufnr, "InlayHintsShow", function()
+          require("lsp-inlayhints").show(bufnr)
+        end, {
+          force = true,
+          desc = "Fetch and show the lsp inlay hints",
+        })
+
+        vim.api.nvim_buf_create_user_command(
+          bufnr,
+          "InlayHintsToggle",
+          function()
+            require("lsp-inlayhints").toggle()
+          end,
+          {
+            force = true,
+            desc = "Toggle the lsp inlay hints",
+          }
+        )
+
+        vim.api.nvim_buf_create_user_command(
+          bufnr,
+          "InlayHintsReset",
+          function()
+            require("lsp-inlayhints").reset(bufnr)
+          end,
+          {
+            force = true,
+            desc = "Clears all hints in the buffer",
+          }
+        )
       end,
     })
   end,
