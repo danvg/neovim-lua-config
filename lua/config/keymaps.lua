5fbad53b92d222e275093195e68127b8aa8950a4
Dan Lee Vazquez Garcia <dava1000@student.miun.se>
2023-07-09 07:50:49 +0200

    Add additional keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -18,9 +18,13 @@ M.setup = function()
 
   -- Better window navigation
   vim.keymap.set("n", "<C-h>", "<C-w>h")
+  vim.keymap.set("n", "<C-left>", "<C-w>h")
   vim.keymap.set("n", "<C-j>", "<C-w>j")
+  vim.keymap.set("n", "<C-down>", "<C-w>j")
   vim.keymap.set("n", "<C-k>", "<C-w>k")
+  vim.keymap.set("n", "<C-up>", "<C-w>k")
   vim.keymap.set("n", "<C-l>", "<C-w>l")
+  vim.keymap.set("n", "<C-right>", "<C-w>l")
 
   -- Resize windows
   vim.keymap.set("n", "<C-r>k", "<cmd>resize -4<CR>")
