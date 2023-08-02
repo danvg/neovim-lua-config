8e4ec25b9b4b626f04d63eae57b60557bd1878a4
Dan Lee Vazquez Garcia <dava1000@student.miun.se>
2023-08-02 16:14:12 +0200

    Fix pwsh on windows.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -76,10 +76,12 @@ M.setup = function()
 
   -- Shell
   if vim.loop.os_uname().sysname == "Windows_NT" then
-    vim.opt.shell = "pwsh -NoLogo -NoProfile"
-    vim.opt.shellcmdflag = "-command"
-    vim.opt.shellquote = "\\"
+    vim.opt.shell = "pwsh.exe"
+    vim.opt.shellquote = ""
     vim.opt.shellxquote = ""
+    vim.opt.shellcmdflag = "-NoLogo -NoProfile -command "
+    vim.opt.shellpipe = "| Out-File -Encoding UTF8 %s"
+    vim.opt.shellredir = "| Out-File -Encoding UTF8 %s"
   end
 end
 
